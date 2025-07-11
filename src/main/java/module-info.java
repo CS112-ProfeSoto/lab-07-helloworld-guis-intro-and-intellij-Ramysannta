a2e73a44c36ff3e4c9e5d1dfd76716efa9ad11b5
Ramysannta <sannta.hyramy@gmail.com>
2025-07-11 08:35:11 +0000

    finished code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module edu.miracosta.cs112.lab07 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens edu.miracosta.cs112.lab07 to javafx.fxml;
     exports edu.miracosta.cs112.lab07;
 }
